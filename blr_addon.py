6cf8af1902cecf9ce870c89d068f140c884437c0
Toopy <florent@toopy.org>
2018-03-12 11:33:17 +0100

    fixup! Add blender addon script

--- blr_addon.py
+++ blr_addon.py
@@ -10,22 +10,48 @@ bl_info = {
 logger = logging.getLogger(__name__)
 
 
-class Blr(bpy.types.Operator):
-    """Run the BLR Addon"""
-    bl_idname = "development.blr"
-    bl_label = "BLR Addon"
+class BlrMixin(bpy.types.Operator):
+    bl_idname = None
+    bl_label = None
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        blr.cmd.run(context)
+        blr.cmd.run(context, self)
         return {'FINISHED'}
 
+
+class BlrAll(BlrMixin):
+    bl_idname = "development.blr_all"
+    bl_label = "BLR All Addon"
+
+
+class BlrFloor0(BlrMixin):
+    bl_idname = "development.blr_floor0"
+    bl_label = "BLR Floor0 Addon"
+
+
+class BlrFloor1(BlrMixin):
+    bl_idname = "development.blr_floor1"
+    bl_label = "BLR Floor1 Addon"
+
+
+class BlrFloor2(BlrMixin):
+    bl_idname = "development.blr_floor2"
+    bl_label = "BLR Floor2 Addon"
+
+
 def register():
-    bpy.utils.register_class(Blr)
+    bpy.utils.register_class(BlrAll)
+    bpy.utils.register_class(BlrFloor0)
+    bpy.utils.register_class(BlrFloor1)
+    bpy.utils.register_class(BlrFloor2)
 
 
 def unregister():
-    bpy.utils.unregister_class(Blr)
+    bpy.utils.unregister_class(BlrAll)
+    bpy.utils.unregister_class(BlrFloor0)
+    bpy.utils.unregister_class(BlrFloor1)
+    bpy.utils.unregister_class(BlrFloor2)
 
 
 if __name__ == "__main__":
